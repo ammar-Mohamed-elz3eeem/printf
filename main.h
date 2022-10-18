03f3fb7dc397af6eb725b2267764b603a1a5cbef
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-18 11:55:13 +0200

    initialize project structure

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+
+#endif
---
+++ test/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entru point
+ * 
+ * Return: 0 if success, 1 otherwise
+ */
+
+int main(void)
+{
+	
+}
