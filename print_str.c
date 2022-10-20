826322faa08216604ebe732f7cd7f8e1f58bfe99
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:31:38 +0200

    add function to print strings

---
+++ print_str.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_str - this will print str to the standard output
+ * @args: argument list of variadic function
+ * @buff: buff to print data in
+ * @buff_count: count of the current integer in buffer
+ * Return: current buff count
+ */
+
+int print_str(char *buff, va_list args, int buff_count)
+{
+	char *str;
+	int i;
+
+	str = va_arg(args, char *);
+
+	while (str[i] != '\0')
+	{
+		buff[buff_count++] = str[i++];
+	}
+
+	return (buff_count);
+}
