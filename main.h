a53cee0169defe51dc969586429e95baa3dee080
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:33:36 +0200

    update library functions

--- main.h
+++ main.h
@@ -5,5 +5,17 @@
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
+int print_int(char *buff_dest, va_list args, int buff_count);
+int printer(char specefier, va_list args, char *buff, int buff_count);
+void write_buffer(char *buffer, int buff_count);
+int print_char(char *buff, va_list args, int buff_count);
+int print_str(char *buff, va_list args, int buff_count);
+int print_precision(char *buff, va_list args, int buff_count);
+
+typedef struct printer_s
+{
+	char specefier;
+	int (*printer)(char *, va_list, int);
+} printer_t;
 
 #endif
