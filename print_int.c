8c5cbc440d9747a00a02ec3c2a45adb10ad445ce
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:32:27 +0200

    add function to print intgers

---
+++ print_int.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include "limits.h"
+#include <stdio.h>
+
+/**
+ * print_int - prints an integer to the output stream
+ * @args: argument list of variadic function
+ * @buff: buff to print data in
+ * @buff_count: count of the current integer in buffer
+ * Return: current buff count
+ */
+
+int print_int(char *buff_dest, va_list args, int buff_count)
+{
+	int tens = 1;
+	unsigned int tmp;
+	int number;
+
+	number = va_arg(args, int);
+
+	if (number < 0)
+	{
+		buff_dest[buff_count] = '-';
+		number *= -1;
+		buff_count++;
+	}
+	tmp = number;
+
+	if (number == INT_MIN)
+		tmp++;
+
+	while (tmp > 9)
+	{
+		tens *= 10;
+		tmp /= 10;
+	}
+
+	tmp = number;
+	while (tens > 0)
+	{
+		buff_dest[buff_count] = ('0' + tmp / tens);
+		tmp %= tens;
+		tens /= 10;
+		buff_count++;
+	}
+
+	return (buff_count);
+}
