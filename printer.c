b7853c14d94821ebdad3e34f123f8645e2cc0ced
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:32:51 +0200

    add function to choose between printers

---
+++ printer.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * printer - print a charchter to the output stream
+ * @specefier: charchter to specify which specify argument type
+ */
+
+int printer(char specefier, va_list args, char *buff, int buff_count)
+{
+	int i;
+	printer_t printers[] = {
+	    {'i', print_int},
+	    {'d', print_int},
+	    {'c', print_char},
+	    {'s', print_str},
+	    {'%', print_precision},
+	    {'\0', NULL}};
+
+	for (i = 0; printers[i].specefier != '\0'; i++)
+	{
+		if (printers[i].specefier == specefier)
+		{
+			buff_count = printers[i].printer(buff, args, buff_count);
+		}
+	}
+
+	return buff_count;
+}
