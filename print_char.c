96fed3203d363d0b079ceec41782b50b7a9b2f15
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:31:52 +0200

    add function to print chars

---
+++ print_char.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_char - this will print char to the standard output
+ * @args: argument list of variadic function
+ * @buff: buff to print data in
+ * @buff_count: count of the current integer in buffer
+ * Return: current buff count
+ */
+
+int print_char(char *buff, va_list args, int buff_count)
+{
+	char ch = va_arg(args, int);
+
+	buff[buff_count++] = ch;
+
+	return buff_count;
+}
