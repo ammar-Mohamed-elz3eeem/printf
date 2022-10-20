70d8dc0c83a517cecf429cb92b9f02346267a452
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-20 21:30:03 +0200

    add main printf function

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _printf - print chars to stdout in formatted way
+ * @format - string of fomatted chars to be printed
+ * Return: 1 if printing is successful
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int buff_count = 0;
+	int prev_buff_count = 0;
+	va_list args;
+	char buffer[1024];
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] != '\0')
+			{
+				buff_count = printer(format[i], args, buffer, buff_count);
+			}
+		}
+		else
+		{
+			buffer[buff_count] = format[i];
+			buff_count++;
+		}
+		i++;
+	}
+	write_buffer(buffer, buff_count);
+	return buff_count;
+}
