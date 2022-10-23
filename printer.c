9ccfa955bad153ceb50573f56e0b538d6ac9c32e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-10-23 13:50:35 +0200

    add printer for hexadecimals

--- printer.c
+++ printer.c
@@ -21,6 +21,7 @@ int printer(char specefier, va_list args, char *buff, int buff_count)
 	    {'u', print_unsigned},
 	    {'o', print_octal},
 	    {'%', print_precision},
+	    {'x', print_hexadecimal},
 	    {'\0', NULL}};
 
 	for (i = 0; printers[i].specefier != '\0'; i++)
